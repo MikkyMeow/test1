5e16785c30b4c5e5c956cbc614b57d9fc36a6bbf
Artur Akhmadullin <arturakk2015@gmail.com>
2022-10-29 00:09:19 +0400

    nav base styles

--- src/components/elements/Nav/index.jsx
+++ src/components/elements/Nav/index.jsx
@@ -1,19 +1,35 @@
+import { useState } from "react";
+import { LanguageButtons } from "../../ui/LanguageButtons";
+import styles from "./index.module.css";
+
 export const Nav = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
+
   return (
-    <div class="nav">
-      <span>LOGO</span>
+    <div className={styles.root}>
+      <span className={styles.logo}>LOGO</span>
+      <button
+        className={styles.menuBtn}
+        onClick={() => setIsMenuOpen((prev) => !prev)}
+      >
+        <span />
+        <span />
+        <span />
+      </button>
+      {/* <div className={`${styles.content} ${!isMenuOpen ? "show" : ""}`}> */}
       <nav>
-        <ul>
+        <ul className={`${styles.list} ${isMenuOpen ? "show" : ""}`}>
           <li>Проекты</li>
           <li>О компании</li>
-          <li>Контакты</li>
+          <li className="active">Контакты</li>
         </ul>
       </nav>
-      <div class="languages">
-        <button class="langBtn">RU</button>
-        <button class="langBtn">EN</button>
+      <div className={`${styles.languages} ${isMenuOpen ? "show" : ""}`}>
+        <LanguageButtons />
       </div>
-      <button class="startProject">Начать проект</button>
+      <button className={`${styles.startBtn} ${isMenuOpen ? "show" : ""}`}>
+        Начать проект &#8594;
+      </button>
     </div>
   );
 };
