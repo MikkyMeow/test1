5789616b37027d215c0bc461ba309517a7d474fa
Artur Akhmadullin <arturakk2015@gmail.com>
2022-10-30 16:40:06 +0400

    hover&active to links

--- src/components/ui/input/index.jsx
+++ src/components/ui/input/index.jsx
@@ -1,7 +1,6 @@
 import styles from "./index.module.css";
 
 export const Input = ({ className = "", label, ...rest }) => {
-  // TODO: Добавить required
   return (
     <div className={`${styles.root} ${className}`}>
       <input className={styles.input} placeholder=" " {...rest} />
